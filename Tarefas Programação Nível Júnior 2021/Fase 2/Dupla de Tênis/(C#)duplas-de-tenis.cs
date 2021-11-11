c2d9b273532d9be934384ad8e32df366e01199c3
André Luis dos Santos Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2021-11-11 18:08:42 -0300

    Adição de comentários
    Comentários adicionados para melhor compreensão do código.

--- Tarefas Programação Nível Júnior 2021/Fase 2/Dupla de Tênis/(C#)duplas-de-tenis.cs
+++ Tarefas Programação Nível Júnior 2021/Fase 2/Dupla de Tênis/(C#)duplas-de-tenis.cs
@@ -4,14 +4,20 @@ public class Program
 {
 	public static void Main()
 	{
+		// leitura do nível dos jogadores 1, 2, 3 e 4, respectivamente
 		int jogadorUm = int.Parse(Console.ReadLine());
 		int jogadorDois = int.Parse(Console.ReadLine());
 		int jogadorTres = int.Parse(Console.ReadLine());
 		int jogadorQuatro = int.Parse(Console.ReadLine());
 		
+		/* comparação entre o nível dos jogadores sendo o primeiro com o menor nível
+		e o quarto jogador com o maior nível entre os jogadores */
 		if (jogadorUm <= jogadorDois && jogadorDois <= jogadorTres && jogadorTres <= jogadorQuatro) {
+			// primeira dupla formada com o jogador 1 e 4
 			int duplaUm = jogadorUm + jogadorQuatro;
+			// segunda dupla formada com o jogador 2 e 3
 			int duplaDois = jogadorDois + jogadorTres;
+			// diferença entre a soma do nível dos jogadores
 			int diferenca = duplaUm - duplaDois;
 			
 			Console.WriteLine();
